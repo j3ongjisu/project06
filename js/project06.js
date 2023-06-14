805f8f0269731db923a39ad950476acd977da841
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-06-14 17:19:41 +0900

    Add files via upload

--- js/project06.js
+++ js/project06.js
@@ -1,4 +1,12 @@
 $(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
     $('.mainSlide').slick({
         dots: true,
         arrows: false,
